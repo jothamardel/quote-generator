a656f514b5ffcf1629d5023b54e19891ec72fc92
jothamardel <ardelmbiplang@gmail.com>
2020-10-22 13:13:28 +0100

    Implementing twitter API

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ async function getQuote() {
   try {
     const response = await fetch(proxyUrl + apiUrl);
     const data = await response.json();
-    console.log(data);
     // If Author is blank, add Unknown.
     if (data.quoteAuthor === '') {
       authorText.innerText = 'Unknown';
@@ -33,6 +32,18 @@ async function getQuote() {
   }
 
 }
+
+// Tweet Quote
+function tweetQuote() {
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}&hashtags=theVillageProgrammer`;
+  window.open(twitterUrl, '_blank');
+}
+
+// Event Listners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuote();
